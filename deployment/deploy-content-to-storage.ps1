27f62f2f8d4648ca68ce86f18db87218bfe29b89
la-trejas <trey@linuxacademy.com>
2018-05-07 13:27:11 -0500

    Uploading Deployment Script

--- deployment/deploy-content-to-storage.ps1
+++ deployment/deploy-content-to-storage.ps1
@@ -5,3 +5,7 @@ $destinationUri = "https://$($Env:mycontainer_uri)/$containerName"
 $destinationKey = $Env:STORAGE_KEY
 
 D:\devtools\AzCopy\AzCopy.exe /source:.\DSCFiles /Dest:$destinationUri /DestKey: $destinationUri /SetContentType /S /Y
+if ($LastExitCode -ne 0) { throw "azcopy returned code $($LastExitCode)" }
+
+$storageContext = New-AzureStorageContext -ConnectionString $Env:AzureWebJobsStorage -ErrorAction Stop
+Set-AzureStorageContainerAcl -Container $containerName -Permission Blob -Context $storageContext -ErrorAction Stop
